9387c17a05f7a79a18c407353914b6417c452fbe
Zainuzainab <147390714+Zainuzainab@users.noreply.github.com>
2024-11-05 10:59:29 +0530

    Add files via upload

---
+++ aling.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float f=12.87808;
+    char str[]="zainab hello";
+    int c=12345;
+    printf("%-14f\n",f);
+    printf("%14s\n",str);
+    printf("%14d\n",c);
+}
