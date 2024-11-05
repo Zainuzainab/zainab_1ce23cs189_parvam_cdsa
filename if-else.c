f3a13faf8d8921b66b475b4bb8a44b5fba1f13a3
Zainuzainab <147390714+Zainuzainab@users.noreply.github.com>
2024-11-05 15:23:07 +0530

    Add files via upload

---
+++ if-else.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x=10;
+    if(x<5){
+        printf("x is lesser than 5");
+    }
+    else{
+        printf("x is greater than 5");
+    }
+    return 0;
+}
